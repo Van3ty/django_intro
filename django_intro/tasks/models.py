0c155783c75ca182de01e62f173caadc80fd793a
Van3ty <homiesindabank@gmail.com>
2025-08-12 17:03:11 +0300

    Did some work here and there

--- django_intro/tasks/models.py
+++ django_intro/tasks/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=15) 
     text = models.CharField(max_length=50)
+
+class Categoty(models.Model):
+    name = models.CharField(max_length=20)
--- django_intro/tasks/views.py
+++ django_intro/tasks/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django_intro.tasks.models import Task
 # Create your views here.
 
 
 
 def home(request):
-    items = ''.join(f'<li>{i}</li>' for i in range(4))
+    items = Task.objects.all()
+    item_strings = (f'<li>{t.title}</li>' for t in items)
+    items_string = ''.join(item_strings)
     html = f'''
 <h1>It works</h1>
 <ul>
-    {items}
+    {items_string}
 </ul>
     '''
 
